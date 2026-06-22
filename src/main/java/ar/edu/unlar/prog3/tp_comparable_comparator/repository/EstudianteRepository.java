b7604e11bb4f3eaf78f71a747fb47e2d072e05f1
Florencia Luna <florencialuna025@gmail.com>
2026-06-22 00:30:19 -0300

    Feat: Implementación del modelo Estudiante con orden natural y repositorio en memoria

---
+++ src/main/java/ar/edu/unlar/prog3/tp_comparable_comparator/repository/EstudianteRepository.java
@@ -0,0 +1,33 @@
+package ar.edu.unlar.prog3.tp_comparable_comparator.repository;
+
+import ar.edu.unlar.prog3.tp_comparable_comparator.model.Estudiante;
+import org.springframework.stereotype.Repository;
+import ar.edu.unlar.prog3.tp_comparable_comparator.repository.EstudianteRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class EstudianteRepository {
+
+    private final List<Estudiante> estudiantes = new ArrayList<>();
+
+    public EstudianteRepository() {
+
+        estudiantes.add(new Estudiante("LU-2024-001","Martín Quiroga",8.5,22,18));
+        estudiantes.add(new Estudiante("LU-2024-002","Valeria Díaz",8.5,20,15));
+        estudiantes.add(new Estudiante("LU-2024-003","Facundo Castro",7.2,24,22));
+        estudiantes.add(new Estudiante("LU-2024-004","Camila Torres",9.1,21,24));
+        estudiantes.add(new Estudiante("LU-2024-005","Lucas González",9.1,23,24));
+        estudiantes.add(new Estudiante("LU-2024-006","Agustina López",6.8,19,10));
+        estudiantes.add(new Estudiante("LU-2024-007","Nahuel Herrera",7.5,22,14));
+        estudiantes.add(new Estudiante("LU-2024-008","Florencia Ríos",8.9,25,20));
+        estudiantes.add(new Estudiante("LU-2024-009","Tomás Sosa",6.5,20,12));
+        estudiantes.add(new Estudiante("LU-2024-010","Lucía Fernández",7.8,21,16));
+    }
+
+    public List<Estudiante> obtenerTodos() {
+        return estudiantes;
+    }
+
+}
